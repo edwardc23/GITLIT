fb675279eb9489d4eb72e384428be28df97b86f2
Sterling <sterlingtyson97@gmail.com>
2020-04-12 22:29:41 -0400

    Logger file shell
    Logger file shell for registration, login, etc

---
+++ src/ParentUser.java
@@ -0,0 +1,77 @@
+import java.util.UUID;
+
+public abstract class ParentUser {
+    private UUID userID;
+    private String username;
+    private String encryptedPassword;
+    private String password;
+
+    public ParentUser() {
+        this.userID = UUID.randomUUID();
+        this.username = "";
+        this.encryptedPassword = "";
+        this.password = "";
+    }
+
+    public ParentUser(String username, String password) {
+        this.userID = UUID.randomUUID();
+        this.username = username;
+        this.password = password;
+        this.encryptedPassword = encryptPassword(password);
+    }
+
+    public ParentUser(String userID, String username, String encryptedPassword) {
+        this.userID = UUID.fromString(userID);
+        this.username = username;
+        this.encryptedPassword = encryptedPassword;
+        this.password = decryptPassword(encryptedPassword);
+    }
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+        //System.out.println(Main.userFile);
+    }
+
+    public String encryptPassword(String password) {
+        String result = "";
+        for(int i = 0; i < password.length(); i++) {
+            if(Character.isUpperCase(password.charAt(i))) result += (char)(((int)password.charAt(i) - 62) % 26 + 65);
+            else result +=  (char)(((int)password.charAt(i) - 94) % 26 + 97);
+        }
+        return result;
+    }
+
+    public static String decryptPassword(String password) {
+        String result = "";
+        for(int i = 0; i < password.length(); i++) {
+            if(Character.isUpperCase(password.charAt(i))) result += (char)(((int)password.charAt(i) - 42) % 26 + 65);
+            else result +=  (char)(((int)password.charAt(i) - 74) % 26 + 97);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s:%s:%s",userID.toString(),username,encryptedPassword);
+    }
+}
